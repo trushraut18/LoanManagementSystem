d9661a200f6e5cd5329319e791ec1fede44b09c7
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-23 00:21:18 +0530

    Created Exception Handling Middleware

---
+++ LoanManagementSystem/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace LoanManagementSystem.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger) 
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync( HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Unhandled exception Occurred");
+
+                context.Response.ContentType = "application/json";
+
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = "Something went wrong.",
+                    Detailed = e.Message
+                };
+
+                var json = JsonSerializer.Serialize(response);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
