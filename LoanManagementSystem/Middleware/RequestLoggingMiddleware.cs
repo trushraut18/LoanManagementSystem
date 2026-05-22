257c19c8695a7738c436c6be1f946b3ecbdf652e
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-23 00:26:25 +0530

    created RequestLoggingMiddleware

---
+++ LoanManagementSystem/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace LoanManagementSystem.API.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            _logger.LogInformation(
+                "Incoming Request: {method} {url}",
+                context.Request.Method,
+                context.Request.Path);
+
+            await _next(context);
+
+            stopwatch.Stop();
+
+            _logger.LogInformation(
+    "Outgoing Response: {statusCode} executed in {time} ms",
+    context.Response.StatusCode,
+    stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
