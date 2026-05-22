6ae11a4eb4029812577f86eb963b7f555136d139
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-22 22:28:27 +0530

    Added DTO that will interact with User for Login and Register

---
+++ LoanManagementSystem.Application/DTOs/AuthResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagementSystem.Application.DTOs
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+    }
+}
