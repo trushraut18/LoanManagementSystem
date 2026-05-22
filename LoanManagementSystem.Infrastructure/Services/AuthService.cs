c081e22250ef900676f0b9338a1adfdf3ce7fc73
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-22 23:38:34 +0530

    Added auth service for register user , login user and validate token

---
+++ LoanManagementSystem.Infrastructure/Services/AuthService.cs
@@ -0,0 +1,82 @@
+﻿using LoanManagementSystem.Application.DTOs;
+using LoanManagementSystem.Application.Interfaces;
+using LoanManagementSystem.Application.Repositories;
+using LoanManagementSystem.Domain.Entities;
+using Org.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagementSystem.Infrastructure.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        public AuthService(IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _userRepository = userRepository;
+            _jwtTokenGenerator = jwtTokenGenerator;
+
+        }
+        public async Task<AuthResponseDto> LoginAsync(LoginRequestDto request)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.Email);
+
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            var isPasswordValid = BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash);
+
+            if(!isPasswordValid)
+            {
+                throw new Exception("Incorrect Password");
+            }
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthResponseDto
+            {
+                Token = token,
+                Email = user.Email,
+                Role = user.Role
+            };
+
+        }
+
+        public async Task<AuthResponseDto> RegisterAsync(RegisterRequestDto request)
+        {
+            var existingUser = await _userRepository.GetByEmailAsync(request.Email);
+            if (existingUser != null)
+            {
+                throw new Exception("User already exists");
+            }
+
+            var user = new User
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                Role = "Customer",
+                CreatedDate = DateTime.UtcNow
+            };
+
+            await _userRepository.AddSync(user);
+            await _userRepository.SaveChangesAsync();
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthResponseDto
+            {
+                Token = token,
+                Email = user.Email,
+                Role = user.Role
+            };
+        }
+    }
+}
