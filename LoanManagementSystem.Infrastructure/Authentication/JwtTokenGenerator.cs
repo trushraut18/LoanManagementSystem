5ef8fc05d42e3f30cdd8a836e9ba25829646365b
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-22 23:14:27 +0530

    Added configs for JWT setting and added JwtTokenGenerator class which will generate claims and JWT token

---
+++ LoanManagementSystem.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+﻿using LoanManagementSystem.Application.Interfaces;
+using LoanManagementSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace LoanManagementSystem.Infrastructure.Authentication
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+        public JwtTokenGenerator(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+        public string GenerateToken(User user)
+        {
+            var secret = _configuration["JwtSettings:Secret"];
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+            var credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var claim = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Role,user.Role),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claim,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["JwtSettings: ExpiryMinutes"])),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
