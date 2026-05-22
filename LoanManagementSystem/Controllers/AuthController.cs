b32736040c7ec6e52604ecf7ab75eb5d3f083fa3
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-23 00:01:09 +0530

    Added authoritzation controller and added add authentication services in program

---
+++ LoanManagementSystem/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using LoanManagementSystem.Application.DTOs;
+using LoanManagementSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanManagementSystem.API.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestDto request)
+        {
+            var response = await _authService.RegisterAsync(request);
+            return Ok(response);
+        }
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDto register)
+        {
+            var response = await _authService.LoginAsync(register);
+            if(response == null)
+            {
+                return Unauthorized("Invalid Credentials");
+            }
+            return Ok(response);
+        }
+
+    }
+
+
+}
--- LoanManagementSystem/Program.cs
+++ LoanManagementSystem/Program.cs
@@ -1,20 +1,27 @@
+using LoanManagementSystem.Application.Interfaces;
+using LoanManagementSystem.Application.Repositories;
+using LoanManagementSystem.Infrastructure.Authentication;
+using LoanManagementSystem.Infrastructure.Services;
 using LoanManagementSystem.Persistence.Context;
+using LoanManagementSystem.Persistence.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
-
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Serilog.Log.Logger = new LoggerConfiguration()
+Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateLogger();
 
 builder.Host.UseSerilog();
 
-// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -23,16 +30,54 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+var secret =
+    builder.Configuration["JwtSettings:Secret"];
+
+var key = Encoding.UTF8.GetBytes(secret!);
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer =
+                    builder.Configuration["JwtSettings:Issuer"],
+
+                ValidAudience =
+                    builder.Configuration["JwtSettings:Audience"],
+
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(key)
+            };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
