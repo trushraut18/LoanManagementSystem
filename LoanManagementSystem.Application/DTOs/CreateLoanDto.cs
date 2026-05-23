db1de12a26bd8b6852aa4d2cfb258543087a166e
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-23 08:58:16 +0530

    Added Create Loan DTO Created LoanController

---
+++ LoanManagementSystem.Application/DTOs/CreateLoanDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagementSystem.Application.DTOs
+{
+    public class CreateLoanDto
+    {
+        public decimal Amount { get; set; }
+        public int DurationInMonths { get; set; }
+        public decimal InterestRate { get; set; }
+    }
+}
---
+++ LoanManagementSystem/Controllers/LoansController.cs
@@ -0,0 +1,48 @@
+﻿using LoanManagementSystem.Application.DTOs;
+using LoanManagementSystem.Domain.Entities;
+using LoanManagementSystem.Persistence.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanManagementSystem.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class LoansController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public LoansController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+  
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> CreateLoan(CreateLoanDto request)
+        {
+            var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            var loan = new Loan
+            {
+                Amount = request.Amount,
+                DurationInMonths = request.DurationInMonths,
+                InterestRate = request.InterestRate,
+                Status = "Pending",
+                UserId = Convert.ToInt32(userId),
+                CreatedDate = DateTime.UtcNow
+            };
+
+            await _context.Loans.AddAsync(loan);
+            await _context.SaveChangesAsync();  
+            return Ok("");
+        }
+ 
+        [Authorize(Roles = "Admin")]
+        [HttpGet("all")]
+        public IActionResult GetAllLoans()
+        {
+            var loans = _context.Loans.ToList();
+            return Ok(loans);
+        }
+    }
+}
