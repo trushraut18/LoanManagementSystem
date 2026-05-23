9e00929fbd48584440542501ea0cc84d226ffdbb
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-23 09:34:08 +0530

    Added RefreshTokenGenerator and added it in Auth Response DTO

--- LoanManagementSystem.Application/DTOs/AuthResponseDto.cs
+++ LoanManagementSystem.Application/DTOs/AuthResponseDto.cs
@@ -9,6 +9,7 @@ namespace LoanManagementSystem.Application.DTOs
     public class AuthResponseDto
     {
         public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty; 
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
 
---
+++ LoanManagementSystem.Infrastructure/Authentication/RefreshTokenGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagementSystem.Infrastructure.Authentication
+{
+    public static class RefreshTokenGenerator
+    {
+        public static string Generate()
+        {
+            var randomBytes = new byte[64];
+            
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomBytes);
+            return Convert.ToBase64String(randomBytes);
+        }
+    }
+}
