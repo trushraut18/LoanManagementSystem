63572ed37dbcd67646d243b6fa8fab067df3b02d
TRUSHNA RAUT <trushraut18@gmail.com>
2026-05-24 08:19:59 +0530

    Added DTO for Dashboard analytic view and loan summary

---
+++ LoanManagementSystem.Application/DTOs/DashboardAnalyticsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagementSystem.Application.DTOs
+{
+    public class DashboardAnalyticsDto
+    {
+        public int TotalLoans { get; set; }
+        public decimal TotalLoanAmount { get; set; }
+        public int ApprovedLoans { get; set; }
+        public int PendingLoans { get; set; }
+    }
+}
---
+++ LoanManagementSystem.Application/DTOs/LoanSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagementSystem.Application.DTOs
+{
+    public class LoanSummary
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public decimal InterestRate { get; set; }
+        public int DurationInMonths { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+    }
+}
